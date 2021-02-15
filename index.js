157da71b6f5c334fc1f90b600dfca57e49b70dff
AzizKammoun <44702870+aziz191546@users.noreply.github.com>
2021-02-15 21:18:27 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+// NOTE: DON'T USE THIS FOR ACTUAL SERVERS WITH BACKEND!
+// HAVE ACTUAL PATHS!
+const express = require("express");
+const app = express();
+
+app.use(express.static(__dirname + "/public"), (_, res, next) => {
+  res.status(404)
+  res.sendFile(__dirname + "/public/404.html")
+});
+
+
+
+app.listen(8080);
